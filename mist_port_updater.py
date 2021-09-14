89d703997a8a41b27962e52e3f7b016aa085d6a5
dan opfer <omgeewtfbbq@gmail.com>
2021-09-14 11:45:08 -0700

    Update mist_port_updater.py

--- mist_port_updater.py
+++ mist_port_updater.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#steps:
+# steps:
 # 1 - log into ios device
 # 2 - find access points using lldp
 # 3 - grab mac address & port, store as var
@@ -10,28 +10,35 @@
 # 7 - update port desc with name of AP
 
 import requests
-import getpass 
-import json 
+import getpass
+import json
 import napalm
 import re
 import time
 
-#api creds
+# api creds
 email = input('Mist E-mail: ')
-p=getpass.getpass('Enter Mist Password: ')
+p = getpass.getpass('Enter Mist Password: ')
 
+# ssh creds
+user = input('SSH Username: ')
+passwd = getpass.getpass('SSH Password: ')
 
-mist_raw = requests.get('https://api.mist.com/api/v1/orgs/{{ org_id }}', auth=(email, p))  
+# api call
+mist_raw = requests.get(
+    'https://api.mist.com/api/v1/orgs/{{ org_id }}', auth=(email, p))
 mist = mist_raw.json()
 
 with open('idf_list_named') as idf_list:
     idf_list_named = json.loads(idf_list.read())
 
-#blank dictionary to be used for storing interface id[key] and the name of the AP[value]
+# blank dictionary to be used for storing interface id[key] and the name of the AP[value]
 dicts = {}
 
-#loop through all detected devices, pull mac address and 
-#removes "." before passing it to the mistsearch function
+# loop through all detected devices, pull mac address and
+# remove "." before passing it to the mistsearch function
+
+
 def extractmaclldp(lldp, mist):
     print('cleaning mac address')
     for interface in lldp:
@@ -40,8 +47,10 @@ def extractmaclldp(lldp, mist):
             cleanmac = re.sub('\.', '', str(mac))
             mistsearch(cleanmac, mist, interface, dicts)
 
-#takes the AP mac and tries to find a match in the data from mist
-#if match is found, store key value pair in dicts 
+# takes the AP mac and tries to find a match in the data from mist
+# if match is found, store key value pair in dicts
+
+
 def mistsearch(mac, mist, interface, dicts):
     for device in mist:
         try:
@@ -55,32 +64,32 @@ def mistsearch(mac, mist, interface, dicts):
             print("*" * 50)
             continue
 
-def mainfunct(idf_list_named, mist):
+
+def mainfunct(idf_list_named, mist, user, passwd):
     for device_name, ip in idf_list_named.items():
         try:
-            user = 'username'
-            passwd = 'password'
             print("*" * 50)
             print('trying: '+device_name)
-            #connect to device using napalm to get lldp neighbors 
+            # connect to device using napalm to get lldp neighbors
             ios_driver = napalm.get_network_driver('ios')
             ios = ios_driver(hostname=ip, username=user, password=passwd)
             ios.open()
             lldp = ios.get_lldp_neighbors()
-            #create new dictionary of interfaces and associated ap_name 
+            # create new dictionary of interfaces and associated ap_name
             extractmaclldp(lldp, mist)
             print('updating interface descriptions')
-            #loop through new dictionary of interface:ap_name pairs
+            # loop through new dictionary of interface:ap_name pairs
             for key in dicts:
                 try:
                     print('updating interface ' + key)
-                    ios.cli(["configure terminal\ninterface "+key+"\ndescription "+dicts[key]+"\nend\n"])
+                    ios.cli(["configure terminal\ninterface "+key +
+                            "\ndescription "+dicts[key]+"\nend\n"])
                     time.sleep(2)
                 except Exception as e:
                     print(e)
                     print("*" * 50)
                     continue
-            #save config
+            # save config
             print('Saving config \n')
             ios.cli(["write memory\n"])
             time.sleep(3)
@@ -88,11 +97,13 @@ def mainfunct(idf_list_named, mist):
             ios.close()
             dicts.clear()
             print("*" * 50)
-            #disconnect from ios device
+            # disconnect from ios device
         except Exception as e:
             dicts.clear()
             print(e)
             print("*" * 50)
             continue
 
-mainfunct(idf_list_named, mist)
+
+if '__name__' == '__main__':
+    mainfunct(idf_list_named, mist, user, passwd)
